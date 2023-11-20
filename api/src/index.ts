fe72e29c264764e95d965d49477e561cb0184a07
alexp <alexandrupi@payoneer.com>
2023-11-20 02:44:08 +0200

    fix deploy

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,11 @@
 import { AppDataSource } from "./data-source"
-import { Product } from "./entity/Product"
 import express from "express"
 import morgan from "morgan"
 import authRoutes from "./routes/auth"
-import trim from "./middlewares/trim";
 import cookieParser from 'cookie-parser'
+import cors from 'cors';
+
+
 require('dotenv').config();
 
 
@@ -12,21 +13,25 @@ const app = express()
 
 app.use(express.json())
 app.use(morgan('dev'))
-app.use(trim)
+// app.use(trim)
 app.use(cookieParser())
 
-app.get( "/" , (_, res) => res.send("Hello"))
+app.get( "/" , (_, res) => res.json("Hello"))
 app.use("/api/auth", authRoutes)
+app.use(cors());
 
 const portDev = 5001
 const port = process.env.PORT || portDev;
 
 app.listen(port, async () => {
-    console.log('Server running at 5001')
+    console.log('Server running at:' + port)
     try {
         await AppDataSource.initialize();
+
     }catch (err) {
         console.log(err)
     }
 })
+
+export default app;
 
