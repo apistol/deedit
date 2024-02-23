b8d5f30cadf57c329e5bc8813e92aee376dd8152
luca1777 <luca.alexandru17@yahoo.com>
2024-02-23 15:23:14 +0200

    set login page

--- client/app/(auth)/login/page.tsx
+++ client/app/(auth)/login/page.tsx
@@ -1,28 +1,23 @@
 'use client';
+import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import * as Yup from 'yup';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { signUpRegister } from '../utils/utils';
+import { login, signUpRegister } from '../../../utils/utils';
 
-interface FormDataRegister {
-  name: string;
+interface FormDataLogin {
   email: string;
   password: string;
 }
 
 const INITIAL_FORM_STATE = {
-  name: '',
   email: '',
   password: '',
-  confirmPassword: '',
-  termsAndPolicy: false,
 };
 
 const FORM_VALIDATION = Yup.object().shape({
-  name: Yup.string().required('Required'),
   email: Yup.string()
     .email('Invalid email.')
     .required('Required')
@@ -35,29 +30,23 @@ const FORM_VALIDATION = Yup.object().shape({
     .min(2, 'Too Short!')
     .max(50, 'Too Long!')
     .required('Required'),
-  confirmPassword: Yup.string()
-    .oneOf([Yup.ref('password'), null], 'Passwords must match')
-    .required('Required'),
-  termsAndPolicy: Yup.boolean()
-    .oneOf([true], 'The terms and conditions must be accepted.')
-    .required('Required'),
 });
 
-export default function Register() {
-  const [formData, setFormData] = useState<FormDataRegister | null>(null);
+export default function Login() {
+  const [formData, setFormData] = useState<FormDataLogin | null>(null);
   const [formCompleted, setFormCompleted] = useState(false);
 
   useEffect(() => {
     if (formCompleted && formData) {
-      const submitRegister = async () => {
-        const register = await signUpRegister(formData);
-        if (register) {
-          console.log('Register created successfully:', register);
+      const submitLogin = async () => {
+        const loginUser = await login(formData);
+        if (loginUser) {
+          console.log('Login successfully:', loginUser);
         } else {
-          console.log('Error creating register');
+          console.log('Error login');
         }
       };
-      submitRegister();
+      submitLogin();
     }
   }, [formCompleted, formData]);
 
@@ -67,16 +56,18 @@ export default function Register() {
   };
 
   return (
-    <div className="flex flex-row justify-center items-center min-h-screen">
+    <div className="flex flex-col lg:flex-row justify-center items-center min-h-screen">
       <Head>
         <title>Register</title>
       </Head>
-
-      <div className="w-full lg:w-1/2 max-w-sm mx-auto mb-24 px-8 sm:px-0">
+      <div className="mx-auto lg:hidden mb-20">
+        <p className=" text-8xl font-black bg-beautiful-gradient text-transparent bg-clip-text">
+          deedit.
+        </p>
+      </div>
+      <div className="flex flex-col w-full lg:w-1/2 max-w-sm mx-auto mb-24 px-8 sm:px-0">
         <div className="mb-10">
-          <p className="font-black text-3xl">
-            Hai sa ne zici povestea aia Blana!
-          </p>
+          <p className="font-black text-3xl">Hai sa povestim iar!</p>
         </div>
         <div className="">
           <Formik
@@ -87,22 +78,6 @@ export default function Register() {
             }}
           >
             <Form>
-              <div className="py-4">
-                <label htmlFor="name" className="block font-semibold">
-                  Username
-                </label>
-                <Field
-                  name="name"
-                  placeholder="Please enter your username"
-                  type="text"
-                  className="mt-1 w-full border border-gray-300 rounded-md py-2 px-2 text-sm"
-                />
-                <ErrorMessage
-                  name="name"
-                  component="div"
-                  className="text-red-600"
-                />
-              </div>
               <div className="pb-4">
                 <label htmlFor="email" className="block font-semibold">
                   Email
@@ -119,7 +94,7 @@ export default function Register() {
                   className="text-red-600"
                 />
               </div>
-              <div className="pb-4">
+              <div className="pb-8">
                 <label htmlFor="password" className="block font-semibold">
                   Password
                 </label>
@@ -135,48 +110,12 @@ export default function Register() {
                   className="text-red-600"
                 />
               </div>
-              <div className="pb-4">
-                <label
-                  htmlFor="confirmPassword"
-                  className="block font-semibold"
-                >
-                  Confirm Password
-                </label>
-                <Field
-                  name="confirmPassword"
-                  placeholder="Please confirm your password"
-                  type="text"
-                  className="mt-1 w-full border border-gray-300 rounded-md py-2 px-2 text-sm"
-                />
-                <ErrorMessage
-                  name="confirmPassword"
-                  component="div"
-                  className="text-red-600"
-                />
-              </div>
-              <div className="pb-4">
-                <label className="inline-flex items-center mt-3">
-                  <Field
-                    type="checkbox"
-                    name="termsAndPolicy"
-                    className="form-checkbox h-5 w-5 text-gray-600"
-                  />
-                  <span className="ml-2 text-gray-700">
-                    I agree to the Terms of Service
-                  </span>
-                </label>
-                <ErrorMessage
-                  name="termsAndPolicy"
-                  component="div"
-                  className="text-red-600"
-                />
-              </div>
               <div className="">
                 <button
                   type="submit"
                   className="w-full bg-beautiful-gradient hover:bg-red-600 text-white font-semibold py-2 px-2 text-sm rounded-md"
                 >
-                  Signup
+                  SignIn
                 </button>
               </div>
             </Form>
@@ -184,9 +123,9 @@ export default function Register() {
         </div>
         <div className="flex w-full my-4">
           <p className="font-semibold">
-            Already a deeder?{' '}
+            Nu ai un cont?{' '}
             <span className="text-blue-600 hover:underline">
-              <Link href="/">Sign in</Link>
+              <Link href="/register">Sign up</Link>
             </span>
           </p>
         </div>
--- client/utils/utils.tsx
+++ client/utils/utils.tsx
@@ -12,3 +12,16 @@ export const signUpRegister = async (formData) => {
     console.error('Registration error:', error.response?.data || error.message);
   }
 };
+
+export const login = async (loginData) => {
+  try {
+    const endpointLogin = 'http://localhost:3000/auth/login';
+
+    const resData = await axios.post(endpointLogin, loginData);
+
+    console.log('JWT Token', resData.data.token);
+    return resData;
+  } catch (error) {
+    console.error('Registration error:', error.response?.data || error.message);
+  }
+};
