0db758cb5834f790df6d8ae1be2e9e48b1d243e4
Victor <91851706+ashcsh@users.noreply.github.com>
2024-03-13 13:38:50 +0200

    init native

---
+++ native/app/(tabs)/droguri.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Droguri = () => {
+  return (
+    <View style={{height:"100%", width:"100%", backgroundColor:"black", paddingTop:20}}>
+    <Text style={{color:"white"}}>Droguri</Text>
+  </View>
+  )
+}
+
+export default Droguri
---
+++ native/app/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+import { ScrollView, View, Text } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const Sex = () => {
+    return (
+        <ScrollView>
+
+            <View style={{ height: "100%", width: "100%", backgroundColor: "black", paddingTop: 20 }}>
+                <Text style={{ color: "white" }}>Sex</Text>
+                </View>
+        </ScrollView>
+    )
+}
+
+export default Sex
